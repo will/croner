bc8396026324524a64ee9e3487edb83d8802d1d2
Will Leinweber <will@bitfission.com>
2011-01-04 12:09:12 -0800

    manifest and sinatra reloader

--- croner.rb
+++ croner.rb
@@ -2,9 +2,18 @@ require 'rubygems'
 require 'bundler'
 Bundler.setup
 require 'sinatra'
+require 'sinatra/reloader'
 
 class Croner < Sinatra::Base
+  configure(:development) do
+    register Sinatra::Reloader
+  end
+
   get '/' do
     "hello"
+  end
+
+  post '/heroku/resources' do
+    p params
   end
 end
