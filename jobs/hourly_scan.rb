f964ca9e0d26b570809112e363c6fe215fcb674c
Will Leinweber <will@bitfission.com>
2011-01-04 15:33:57 -0800

    chechpoint for job spec

--- jobs/hourly_scan.rb
+++ jobs/hourly_scan.rb
@@ -2,6 +2,5 @@ class HourlyScan
   @queue = :scans
 
   def self.perform
-    puts 'scan'
   end
 end
--- spec/jobs/hourly_scan_spec.rb
+++ spec/jobs/hourly_scan_spec.rb
@@ -8,6 +8,12 @@ describe HourlyScan do
 end
 
 describe HourlyScan, '.perform' do
+  before(:each) do
+    DB.save_doc :heroku_id => "app123@heroku.com"
+    DB.save_doc :heroku_id => "app888@heroku.com"
+  end
+
   it "should find apps that need to run this hour"
+
   it "should enqueue a job for each"
 end
