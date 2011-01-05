cbeffe07b95179741dcfa1d1a3ad28bb4f0409f7
Will Leinweber <will@bitfission.com>
2011-01-04 23:02:40 -0800

    auto enqueue the first runw

--- models/app.rb
+++ models/app.rb
@@ -5,4 +5,12 @@ class App < CouchRest::Model::Base
   property :plan
   property :last_attempted, Time
   timestamps!
+
+  after_create :enqueue
+
+  private
+
+  def enqueue
+    Resque.enqueue RunAppCron, id
+  end
 end
--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -8,3 +8,12 @@ describe App do
   it { should respond_to(:created_at) }
   it { should respond_to(:updated_at) }
 end
+
+describe App, 'after create' do
+  it 'should enqueue the first run' do
+    Resque.should_receive(:enqueue).once
+    app = App.new
+    app.save
+    app.save
+  end
+end
