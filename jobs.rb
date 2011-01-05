0ad4bc6c0a77e460040ec273abd73e876ed48ce9
Will Leinweber <will@bitfission.com>
2011-01-04 17:38:10 -0800

    try for redis to go url

--- jobs.rb
+++ jobs.rb
@@ -2,3 +2,8 @@ require 'config/env'
 require 'resque'
 require 'jobs/hourly_scan'
 require 'jobs/run_app_cron'
+
+if ENV['REDISTOGO_URL']
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
